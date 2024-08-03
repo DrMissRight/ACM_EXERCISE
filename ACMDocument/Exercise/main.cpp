bbb59b8b4b64a010186ba86390a8c0f52362939e
DrMissRight <114223686+DrMissRight@users.noreply.github.com>
2024-08-03 11:38:25 +0800

    Add files via upload

---
+++ ACMDocument/Exercise/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+using LL = long long;
+using ULL = unsigned long long;
+typedef pair<LL, LL> PII;
+const int inf = 0x3f3f3f3f;
+
+
+void solve(){
+    cout << 1 << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int T = 1;
+    while(T --) solve();
+    return 0;
+}
